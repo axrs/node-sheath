ad6bd4e38c8f8e21c644b9954b403b003860fbbc
Alexander Scott <xander@axrs.io>
2020-06-09 20:10:47 +1000

    License added

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject io.axrs/node-shuck "0.0.0-SNAPSHOT"
   :description "Node and NPM Shell Executor for Clojure"
   :min-lein-version "2.8.1"
+  :url "https://github.com/axrs/node-shuck"
+  :license {:name         "Eclipse Public License - v 2.0"
+            :url          "https://www.eclipse.org/legal/epl-v20.html"
+            :distribution :repo}
   :dependencies [[io.axrs/shuck "0.0.0-SNAPSHOT" :scope "provided"]]
   :clean-targets ["target"]
   :source-paths ["src"]
